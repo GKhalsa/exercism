88dc85a09f3627f0506f31036f74440ce0b67388
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-07-04 16:31:55 -0600

    add finished bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,43 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+
+  if(allSpaces(input)){
+  return 'Fine. Be that way!';
+} else if(allCaps(input)){
+    if (allNum(input)){
+      return 'Whatever.';
+    } else if (onlyNumAndQuestion(input)){
+      return 'Sure.';
+    } else {
+      return 'Whoa, chill out!';
+    }
+  } else if(isAQuestion(input)){
+    return 'Sure.';
+  } else {
+    return 'Whatever.';
+  }
+
+  function onlyNumAndQuestion(input){
+    return /^\d+$/.test(input.replace('?',''));
+  }
+
+  function allSpaces(input){
+    return (input.replace(/ /g,'')) === '';
+  }
+
+  function isAQuestion(input){
+    return input.slice(-1) === '?';
+  }
+
+  function allCaps(input){
+    return input.toUpperCase() === input;
+  }
+
+  function allNum(input){
+    return /^\d+$/.test(input.replace(/,/g,''));
+  }
+
 };
 
 module.exports = Bob;
